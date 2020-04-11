95a244a3e9c3d098b283151a2063f4942ec9ac68
DunaevViktor <rulit.ivan@yandex.ru>
2020-04-11 20:29:29 +0300

    feat: add index.js, Procfile, package.json

---
+++ index.js
@@ -0,0 +1,7 @@
+const TelegramBot = require("node-telegram-bot-api"),
+  port = 5000,
+  host = "0.0.0.0",
+  externalUrl = "https://expense-wiki.herokuapp.com/",
+  token = process.env.TOKEN;
+
+console.log(token);
